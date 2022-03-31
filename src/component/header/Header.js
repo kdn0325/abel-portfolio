1674aa6d1fb6976e30ea8f02682b0d782b7ea2f7
kdn0325 <sexy-ehdsud@nate.com>
2022-03-31 18:54:16 +0900

    header-scroll

--- src/component/header/Header.js
+++ src/component/header/Header.js
@@ -1,15 +1,27 @@
+import { useRef } from "react";
+import { useEffect } from "react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import styles from "./header.module.scss"
 
 const Header = () => {
-    const [isScrolled,setIsScrolled] = useState(false);
-    window.onscroll=()=>{
-        setIsScrolled(window.pageYOffset===0 ? false : true)
-        return () => (window.onscroll = null);
-    }
+    const headerRef = useRef(null);
+
+    useEffect(()=>{
+        const shrinkHeader = () =>{
+            if(document.body.scrollTop > 100 || document.documentElement.scrollTop > 100){
+                headerRef.current.classList.add(`${styles.scrollheader}`)
+            }else{
+                headerRef.current.classList.remove(`${styles.scrollheader}`)
+            }
+        }
+        window.addEventListener("scroll",shrinkHeader);
+        return()=>{
+            window.removeEventListener("scroll",shrinkHeader);
+        }
+    },[])
     return(
-        <div className={`${isScrolled ? `${styles.header} ${styles.scrollheader}` : styles.header}`}>
+        <div className={styles.header} ref={headerRef}>
             <div className={styles.header_wrap}>
                 <h1 className={styles.logo}>
                     <a href="/"><img src={require(`../../Assets/img/portfolio/abel_title.png`)} alt="logo"/></a>
