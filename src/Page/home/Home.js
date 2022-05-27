d4bccede9faf60fc43c187a2f6586a6c5c207b6e
kdn0325 <sexy-ehdsud@nate.com>
2022-05-27 19:13:30 +0900

    sementic_tag_update

--- src/Page/home/Home.js
+++ src/Page/home/Home.js
@@ -5,7 +5,9 @@ const Home = () => {
     return (
         <div>
             <section className={styles.home}>
-                <h1 className={styles.home_greeting}>안녕하세요 <strong>Front-end-Developer Abel</strong>의 포트폴리오입니다</h1>
+                <article>
+                    <h1 className={styles.home_greeting}>안녕하세요 <strong>Front-end-Developer Abel</strong>의 포트폴리오입니다</h1>
+                </article>
             </section>
         </div>
     );
