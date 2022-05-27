d5a43a9dec3adbf848f766b374e3ad1bed5678aa
kdn0325 <sexy-ehdsud@nate.com>
2022-05-27 14:40:04 +0900

    global-reset-install

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import { BrowserRouter} from "react-router-dom";
+import { Reset } from 'styled-reset'
 import styles from "./App.module.scss";
 import Header from './component/header/Header';
 import Footer from './component/footer/Footer';
 import Main from './Main';
 
+
 const App = () => {
     return (
     <BrowserRouter>
         <div className={styles.wrap}>
+            {/* Reset 초기화 05/27 */}
+            <Reset/>
             <Header/>
             <Main/>
             <Footer/>
