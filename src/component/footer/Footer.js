adf687c7fdef62a3389b95444452f72c45a371a8
kdn0325 <sexy-ehdsud@nate.com>
2022-05-27 15:59:29 +0900

    address-update

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import styles from "./Footer.module.scss";
 
+// Footer 추가  - 05/22
 const Footer = () => {
     const [date,setDate] = useState();
     useEffect(()=>{
@@ -9,7 +10,9 @@ const Footer = () => {
     return (
         <footer className={styles.footer}>
             <div className={styles.footer_wrap}>
-                <address className={styles.address}>{date}. Abel's Portfolio All rights reserved</address>
+                <address className={styles.address}>
+                Copyright© {date} Abel's Portfolio. All rights reserved
+                </address>
             </div>
         </footer>
     );
