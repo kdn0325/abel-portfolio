0cdea4ceabdf6d528a58374f8e756ba8484e4123
kdn0325 <sexy-ehdsud@nate.com>
2022-06-02 17:17:19 +0900

    update

--- src/Page/home/Home.js
+++ src/Page/home/Home.js
@@ -6,7 +6,7 @@ const Home = () => {
         <div>
             <section className={styles.home}>
                 <article>
-                    <h1 className={styles.home_greeting}>안녕하세요 <strong>Front-end-Developer Abel</strong>의 포트폴리오입니다</h1>
+                    <h1 className={styles.home_greeting}>안녕하세요. 배움을 통해 더 성장하고 싶은 <strong>UI 개발</strong>&nbsp;및&nbsp;<strong>Front-end 개발자&nbsp;</strong>Abel의 포트폴리오입니다.</h1>
                 </article>
             </section>
         </div>
